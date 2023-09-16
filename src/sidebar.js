afa8eb1c1b471b58149d2d166bc97fd75cf8364f
SameerSul <sameerjuin@gmail.com>
2023-09-16 17:19:33 -0400

    searchbar.css

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,17 +1,24 @@
-// Sidebar.js
-
-import React from 'react';
+import React, { useState } from 'react';
 import './sidebar.css';
 
 const Sidebar = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
-    <div className="sidebar">
+    <div className={`sidebar ${isSidebarOpen ? 'open' : 'closed'}`}>
+      <div className="toggle-button" onClick={toggleSidebar}>
+        {isSidebarOpen ? 'Collapse' : 'Expand'}
+      </div>
       <div className="logo">Your Logo</div>
       <ul className="nav-links">
-        <li>Home</li>
-        <li>About</li>
-        <li>Services</li>
-        <li>Contact</li>
+        <button><li>Home</li></button>
+        <button><li>Feed</li></button>
+        <button><li>Notifications</li></button>
+        <button><li>Messages</li></button>
       </ul>
     </div>
   );
